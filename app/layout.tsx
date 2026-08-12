fe13a4956a542b483d4f9b18e92457cf5ef2ec8d
Mohan <mohanprasath210607@gmail.com>
2026-08-12 18:50:33 +0530

    Add favicon and og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,20 +5,38 @@ export const metadata: Metadata = {
 	title: 'HH Goa 2026 Poster Generator | Build in Goa',
 	description:
 		'Create your customized Hacker House Goa 2026 builder poster and squad cards for social sharing.',
+	metadataBase: new URL('https://hhgoa.taskdrift.in'),
+	icons: {
+		icon: [
+			{ url: '/icon.png', type: 'image/png' },
+			{ url: '/favicon.ico' },
+		],
+		shortcut: '/favicon.ico',
+		apple: '/apple-icon.png',
+	},
 	openGraph: {
-		title: 'HH Goa 2026 Poster Generator',
+		title: 'HH Goa 2026 Poster Generator | Build in Goa',
 		description:
 			'Create your customized Hacker House Goa 2026 builder poster and squad cards.',
 		url: 'https://hhgoa.taskdrift.in',
 		siteName: 'Hacker House Goa 2026',
+		images: [
+			{
+				url: '/og-image.png',
+				width: 1200,
+				height: 630,
+				alt: 'Hacker House Goa 2026',
+			},
+		],
 		locale: 'en_US',
 		type: 'website',
 	},
 	twitter: {
 		card: 'summary_large_image',
-		title: 'HH Goa 2026 Poster Generator',
+		title: 'HH Goa 2026 Poster Generator | Build in Goa',
 		description:
 			'Create your customized Hacker House Goa 2026 builder poster and squad cards.',
+		images: ['/og-image.png'],
 	},
 };
 
